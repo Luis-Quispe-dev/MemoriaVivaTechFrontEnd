601b2dc6b5134388ebe06976a7cbf555f91ed7f2
ErickRodriguez1705 <erick.rodriguez.villasante@gmail.com>
2026-07-01 19:51:43 -0500

    Creacion de css de inicio adulto mayor y css de registrar recuerdo texto

---
+++ src/app/componentes/inicio-adulto-mayor/inicio-adulto-mayor.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inicio-adulto-mayor',
+  imports: [],
+  templateUrl: './inicio-adulto-mayor.html',
+  styleUrl: './inicio-adulto-mayor.css',
+})
+export class InicioAdultoMayor {}
