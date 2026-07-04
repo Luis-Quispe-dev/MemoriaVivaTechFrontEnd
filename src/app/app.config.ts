cbd70c9ade2c0a9f7f2441113d674528bdebe25b
Luis-Quispe-dev <u20231b869@upc.edu.pe>
2026-07-04 09:41:37 -0500

    Agregando Lenguajes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,9 +2,17 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 import {provideHttpClient, withInterceptors} from '@angular/common/http';
 import {provideAnimations} from '@angular/platform-browser/animations';
-
-import { routes } from './app.routes';
 import {authInterceptor} from './interceptors/auth-interceptor';
+import {provideTranslateService} from '@ngx-translate/core';
+import {provideTranslateHttpLoader} from '@ngx-translate/http-loader';
+import { routes } from './app.routes';
+
+const getInitialLang = (): string => {
+  if (typeof window !== 'undefined' && window.localStorage) {
+    return localStorage.getItem('soulstory_lang') || 'Español';
+  }
+  return 'Español';
+};
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -12,5 +20,13 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideHttpClient(withInterceptors([authInterceptor])),
     provideAnimations(),
+    provideTranslateService({
+      loader: provideTranslateHttpLoader({
+        prefix: '/assets/i18n/',
+        suffix: '.json'
+      }),
+      fallbackLang: 'Español',
+      lang: getInitialLang()
+    })
   ]
 };
